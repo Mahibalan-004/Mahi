cc7e8bb0d3b86d17f354b4924b588083245c3d58
Mahi <mahibalan480@gmail.com>
2025-05-16 15:46:25 -0700

    first commit

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ b="Gobi"
 console.log(b)
 const c="GASC"
 console.log(c)
+function abc(){
+    // var a=10
+    // var b=20
+    console.log(a)
+}
+abc()
+function ab(a,b){
+    var c=a+b
+    console.log(c)
+}
+// ab(20,30)
+var d=[20,'Gobi',34.4]
+// var d[1]="Thiran"
+console.log(d[1])
+function check(){
+    var arr = []
+   var d=[20,'Gobi',34.4]
+   for(var i=0;i<=d.length;i++){
+    // console.log(d)
+   console.log(d[i])
+   }
+}
+check()
