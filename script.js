6ddc183ad23901cf88fb2a449a53ca0d9224aed8
Mahi <mahibalan480@gmail.com>
2025-05-17 12:31:53 -0700

    Day 4

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function abc(){
 // abc()
 function ab(a,b){
     var c=a+b
-    console.log(c)
+    console.log(`as ${c}`)
 }
 // ab(20,30)
 var d=[20,'Gobi',34.4]
@@ -30,4 +30,46 @@ function check(){
    }
 }
 // check()
-ab(40,50)
+// ab(40,50)
+/*
+var n=[1,2,3,4,5]
+var even=[]
+var odd=[]
+for(i=0;i<n.length;i++){
+if(n[i]%2==0){
+    // console.log("number is even")
+    even.push(n[i])
+    
+}
+else{
+    // console.log("Not a even number")
+    odd.push(n[i])
+}
+}
+console.log(even)
+console.log(odd)
+*/
+function check() {
+    var arr = [10, 3, 'cdm laksd', 50]; 
+    var even = [];
+    var odd = [];
+    var strings = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number') {
+            if (arr[i] % 2 === 0) {
+                even.push(arr[i]);
+            } else {
+                odd.push(arr[i]);
+            }
+        } else if (typeof arr[i] === 'string') {
+            strings.push(arr[i]);
+        }
+    }
+
+    console.log("Even numbers:", even);
+    console.log("Odd numbers:", odd);
+    console.log("Strings:", strings);
+}
+
+check();
