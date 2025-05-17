640ae46109e2ba74a7fadd42537451b8f21e153c
Mahi <mahibalan480@gmail.com>
2025-05-17 10:43:12 -0700

    Day 3

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 var a=10
 a="Mahi"
 var a=30.3
-console.log(a)
+// console.log(a)
 let b=50
 b="Gobi"
-console.log(b)
+// console.log(b)
 const c="GASC"
-console.log(c)
+// console.log(c)
 function abc(){
     // var a=10
     // var b=20
     console.log(a)
 }
-abc()
+// abc()
 function ab(a,b){
     var c=a+b
     console.log(c)
@@ -20,7 +20,7 @@ function ab(a,b){
 // ab(20,30)
 var d=[20,'Gobi',34.4]
 // var d[1]="Thiran"
-console.log(d[1])
+// console.log(d[1])
 function check(){
     var arr = []
    var d=[20,'Gobi',34.4]
@@ -29,4 +29,5 @@ function check(){
    console.log(d[i])
    }
 }
-check()
+// check()
+ab(40,50)
